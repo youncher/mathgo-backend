085394956f8bab76279633a0b8c894d73bd5e195
unknown <dtran550@gmail.com>
2020-11-08 18:20:40 -0800

    Cleaned up route handlers

--- main.py
+++ main.py
@@ -19,43 +19,37 @@ def root():
 def handle_validation():
 	# TODO: Update to accept https requests
 	# Verify user token; Check if existing Math Go user
-	if request.method == 'POST':
-		content = request.get_json()
-		provided_gid = content.get("gid")
-		provided_id_token = content.get("idToken")
+	content = request.get_json()
+	provided_gid = content.get("gid")
+	provided_id_token = content.get("idToken")
 
-		user_status = {}
+	user_status = {}
 
-		# Missing required fields = sign in fail
-		if provided_gid == None or provided_id_token == None:
-			print("Debug: Missing required field: gid or idToken")
-			user_status["loginSuccessful"] = False
-			return json.dumps(user_status), 401
+	# Missing required fields = sign in fail
+	if provided_gid == None or provided_id_token == None:
+		print("Debug: Missing required field: gid or idToken")
+		return "Missing required field: gid or idToken", 401
 
-		# Verify user token
-		valid_google_user = validate_google_user(provided_id_token);
+	# Verify user token
+	valid_google_user = validate_google_user(provided_id_token);
 
-		# Invalid user token
-		if not valid_google_user:
-			user_status["loginSuccessful"] = False
-			return json.dumps(user_status), 401
+	# Invalid user token
+	if not valid_google_user:
+		return "Invalid user token", 401
 
-		user_status["loginSuccessful"] = True
+	# Check if provided_gid is an existing mathgo user datastore
+	query = datastore_client.query(kind=constants.userEntity)
+	query.add_filter('gid', '=', provided_gid)
+	users = list(query.fetch())
 
-		# Check if provided_gid is an existing mathgo user datastore
-		query = datastore_client.query(kind=constants.userEntity)
-		query.add_filter('gid', '=', provided_gid)
-		users = list(query.fetch())
-
-		if (len(users) == 1) and (users[0]["gid"] == provided_gid):
-			user_status["avatar"] = users[0]["avatar"]
-			user_status["existingUser"] = True
-		else:
-			user_status["existingUser"] = False
-
-		return json.dumps(user_status), 200
+	if (len(users) == 1) and (users[0]["gid"] == provided_gid):
+		user_status["avatar"] = users[0]["avatar"]
+		user_status["existingUser"] = True
 	else:
-		return "Method not recognized"
+		user_status["existingUser"] = False
+
+	return json.dumps(user_status), 200
+
 
 # Verify provided ID token with Google
 def validate_google_user(provided_id_token):
@@ -69,22 +63,23 @@ def validate_google_user(provided_id_token):
 
 @app.route('/user/registration', methods=['POST'])
 def handle_registration():
-	if request.method == 'POST':
-		content = request.get_json()
-		# grabs values from JSON sent from mobile application
-		provided_gid = content.get("gid")
-		provided_name = content.get("displayName")
-		provided_avatar = content.get("avatar")
+	content = request.get_json()
+	# grabs values from JSON sent from mobile application
+	provided_gid = content.get("gid")
+	provided_name = content.get("displayName")
+	provided_avatar = content.get("avatar")
 
-		user_status = {"addSuccessful": True}
+	# Error check required fields
+	if provided_gid == None or provided_name == None or provided_avatar == None:
+		logging.info("Debug: Missing required field(s): gid or name or avatar")
+		return "Missing required field: gid, name, or avatar", 401
 
-		# creating a new user to register into database
-		new_user = datastore.entity.Entity(key=datastore_client.key(constants.userEntity))
-		new_user.update({"displayName": provided_name, "gid": provided_gid, "avatar": provided_avatar})
-		datastore_client.put(new_user)
-		return json.dumps(user_status), 201
-	else:
-		return "Method not recognized"
+	# creating a new user to register into database
+	new_user = datastore.entity.Entity(key=datastore_client.key(constants.userEntity))
+	new_user.update({"displayName": provided_name, "gid": provided_gid, "avatar": provided_avatar})
+	datastore_client.put(new_user)
+	return "Added User Successfully", 201
+
 
 
 if __name__ == '__main__':
